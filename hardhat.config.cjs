0fd20f5e14fd8f1636df098cb45ca6983ad0d0c1
Rahul Sajeevan <144695000+PaiGoManh@users.noreply.github.com>
2024-09-13 17:01:21 +0530

    removed secret files

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -5,11 +5,11 @@ module.exports = {
   solidity: "0.8.20",
   networks: {
     infura: {
-      url: `https://sepolia.infura.io/v3/129df6e60dba4464831b233b9bdd3a00`,
-      accounts: [`9fdafbea5f9a2c0412427a77c5127734f384ba995a1767eac047aee2001ca9b9`]
+      url: ``,
+      accounts: [``]
     }
   },
   etherscan: {
-    apiKey: '9QH2X6C4FAE9ATVUQSA9KXIRKFQDQ5HV1R'
+    apiKey: ''
   }
 };
