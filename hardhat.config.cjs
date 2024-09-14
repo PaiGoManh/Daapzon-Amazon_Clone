7fc155206968f343cd22f359d6241dbf19167beb
PaiGoManh <rahulrahulsajeevan007@gmail.com>
2024-09-14 10:56:20 +0530

    updating ui

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,15 +1,16 @@
 require('@nomiclabs/hardhat-ethers');
 require('@nomiclabs/hardhat-etherscan');
+require('dotenv').config(); 
 
 module.exports = {
   solidity: "0.8.20",
   networks: {
     infura: {
-      url: ``,
-      accounts: [``]
+      url: process.env.INFURA_URL,  
+      accounts: [process.env.PRIVATE_KEY]  
     }
   },
   etherscan: {
-    apiKey: ''
+    apiKey: process.env.ETHERSCAN_API_KEY  
   }
 };
--- src/components/DisplayProduct.jsx
+++ src/components/DisplayProduct.jsx
@@ -1,16 +1,40 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { ethers } from 'ethers';
 import abifile from '../abi.json';
-import AddItemForm from './AddProduct'; 
+import AddItemForm from './AddProduct';
+
 const ItemsList = () => {
   const [items, setItems] = useState([]);
   const [provider, setProvider] = useState(null);
   const [contract, setContract] = useState(null);
   const [account, setAccount] = useState('');
-  const [loading, setLoading] = useState(true); 
+  const [loading, setLoading] = useState(false); 
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedSection, setSelectedSection] = useState('');
   const contractAddress = '0xa967B79a88b70Fc7B4F44a5992d7C688170664eC';
   const abi = abifile.abi;
+
+  const sections = ["Men Fashion", "Household Equipments", "Electronics and Equipments"];
+
+  const initializeContract = async () => {
+    try {
+      if (!window.ethereum) {
+        console.error('MetaMask is not installed.');
+        return;
+      }
+
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(contractAddress, abi, provider.getSigner());
+      const accounts = await provider.send('eth_requestAccounts', []);
+      setProvider(provider);
+      setContract(contract);
+      setAccount(accounts[0]);
+      console.log('Account:', accounts[0]);
+
+    } catch (error) {
+      console.error('Error initializing provider and contract:', error);
+    }
+  };
 
   const fetchItems = async () => {
     try {
@@ -18,12 +42,12 @@ const ItemsList = () => {
         console.log('Provider or contract is not set up.');
         return;
       }
-  
+
       setLoading(true); 
 
       const itemCount = await contract.itemCount();
       console.log('Item count:', itemCount.toString());
-  
+
       const itemsArray = [];
       for (let i = 1; i <= itemCount; i++) {
         const item = await contract.getItem(i);
@@ -38,56 +62,112 @@ const ItemsList = () => {
     }
   };
 
-  useEffect(() => {
-    const init = async () => {
-      try {
-        if (!window.ethereum) {
-          console.error('MetaMask is not installed.');
-          return;
-        }
-
-        const provider = new ethers.providers.Web3Provider(window.ethereum);
-        const contract = new ethers.Contract(contractAddress, abi, provider.getSigner());
-        const accounts = await provider.send('eth_requestAccounts', []);
-        setProvider(provider);
-        setContract(contract);
-        setAccount(accounts[0]);
-        console.log('Account:', accounts[0]);
-
-        await fetchItems();
-      } catch (error) {
-        console.error('Error initializing provider and contract:', error);
-      }
-    };
-
-    init();
-  }, []);
-
   const handleBuy = async (itemId, itemCost) => {
     try {
       if (!provider || !contract) return;
-  
+
       const currentUser = await provider.getSigner().getAddress();
       const item = await contract.getItem(itemId);
-  
+
       if (item.owner === currentUser) {
         alert('You cannot buy your own product.');
         return;
       }
-  
+
       const tx = await contract.purchaseItem(itemId, { value: itemCost });
       await tx.wait();
       console.log('Transaction successful:', tx);
-  
+
       await fetchItems(); 
     } catch (error) {
       console.error('Error handling buy:', error);
     }
   };
 
+  const openModal = (section) => {
+    setSelectedSection(section);
+    setIsModalOpen(true);
+  };
+
   return (
     <div className="flex flex-col p-6">
+      {/* Fetch Items Button */}
+      <button
+        onClick={fetchItems}
+        className="md:ml-[90%] text-white bg-orange-700  font-medium rounded-md text-sm px-5 py-2.5 text-center md:w-[120px]"
+      >
+        Fetch Items
+      </button>
 
+      {sections.map((section) => (
+        <div key={section} className="mt-8">
+          <div className="text-2xl font-bold ml-[15%]">
+            {section}
+            <hr className="mt-4 border-2 border-gray w-[800px]" />
+          </div>
+
+          <div className="flex flex-wrap gap-6 mt-4">
+            {loading ? (
+              <div className="w-full text-center py-6">
+                <p className="text-gray-700">Loading...</p>
+              </div>
+            ) : items.length > 0 ? (
+              items
+                .filter((item) => item.category === section)
+                .map((item) => (
+                  <div
+                    key={item.id.toString()}
+                    className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+                  >
+                    <img className="rounded-t-lg" src={item.imageUrl} alt={item.name} />
+                    <div className="p-5">
+                      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                        {item.name}
+                      </h5>
+                      <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Description</p>
+                      <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                        {ethers.utils.formatEther(item.cost.toString())} ETH
+                      </p>
+                      <a
+                        onClick={() => handleBuy(item.id.toString(), item.cost.toString())}
+                        href="#"
+                        className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                      >
+                        Buy
+                        <svg
+                          className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                          aria-hidden="true"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 14 10"
+                        >
+                          <path
+                            stroke="currentColor"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M1 5h12m0 0L9 1m4 4L9 9"
+                          />
+                        </svg>
+                      </a>
+                    </div>
+                  </div>
+                ))
+            ) : (
+              <div className="w-full text-center py-6">
+                <p className="text-gray-700">No items available in {section}.</p>
+              </div>
+            )}
+
+            <button
+              onClick={() => openModal(section)}
+              className=" ml-[15%] mb-4 text-black border border-1 border-black w-[200px] h-[300px] focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+            >
+              <span className="text-9xl">+</span>
+            </button>
+          </div>
+        </div>
+      ))}
 
       {isModalOpen && (
         <div
@@ -116,68 +196,12 @@ const ItemsList = () => {
               </svg>
               <span className="sr-only">Close modal</span>
             </button>
-            <AddItemForm onClose={() => setIsModalOpen(false)} />
+
+            {/* Pass the selected section to the AddItemForm */}
+            <AddItemForm section={selectedSection} onClose={() => setIsModalOpen(false)} />
           </div>
         </div>
       )}
-
-      <div className="flex flex-wrap gap-6">
-        {loading ? (
-          <div className="w-full text-center py-6">
-            <p className="text-gray-700">Loading...</p>
-          </div>
-        ) : items.length > 0 ? (
-          items.map((item) => (
-            <div
-              key={item.id.toString()}
-              className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
-            >
-              <img className="rounded-t-lg" src={item.imageUrl} alt={item.name} />
-              <div className="p-5">
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                  {item.name}
-                </h5>
-                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Description</p>
-                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                  {ethers.utils.formatEther(item.cost.toString())} ETH
-                </p>
-                <a
-                  onClick={() => handleBuy(item.id.toString(), item.cost.toString())}
-                  href="#"
-                  className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                >
-                  Buy
-                  <svg
-                    className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 14 10"
-                  >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M1 5h12m0 0L9 1m4 4L9 9"
-                    />
-                  </svg>
-                </a>
-              </div>
-            </div>
-          ))
-        ) : (
-          <div className="w-full text-center py-6">
-            <p className="text-gray-700">No items available.</p>
-          </div>
-        )}
-      <button
-        onClick={() => setIsModalOpen(true)}
-        className="mb-4 text-black border border-1 border-black w-[200px] h-[350px] focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-      >
-        <span className='text-9xl'> +</span>
-      </button>
-      </div>
     </div>
   );
 };
